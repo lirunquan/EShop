1b5b7c648cf63231c5c88bea0599365ee1907d8a
lirunquan <1617277400@qq.com>
2019-06-26 23:52:36 +0800

    adding api;

--- eshopproject/apps/user/views.py
+++ eshopproject/apps/user/views.py
@@ -3,10 +3,84 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import Clerk, Customer
 from django.http import JsonResponse
+from django.utils import timezone
+import datetime
 # Create your views here.
 
 @csrf_exempt
 def login(request):
-	pass
+	ret = {
+			'result': 0,
+			'msg': ''
+		}
+	if request.method=='POST':
+		data = json.loads(request.body)
+		uname = data['username']
+		pwd = data['password']
+		group = data['group']
+		if group=='Customer':
+			customer = Customer.objects.filter(username=uname)
+			if len(customer)==0:
+				ret['result'] = -2
+				ret['msg'] = 'the customer does not exists.'
+			elif len(customer)==1 :
+				if customer[0].password==pwd :
+					ret['result'] = 1
+					ret['msg'] = 'customer login successfully.'
+				else:
+					ret['result'] = -1
+					ret['msg'] = 'incorrect password.'
+		elif group=='Clerk':
+			clerk = Clerk.objects.filter(username=uname)
+			if len(clerk)==0:
+				ret['result'] = -2
+				ret['msg'] = 'the clerk does not exists.'
+			elif len(clerk)==1 :
+				if clerk[0].password==pwd :
+					ret['result'] = 1
+					ret['msg'] = 'clerk login successfully.'
+				else:
+					ret['result'] = -1
+					ret['msg'] = 'incorrect password.'
+	else:
+		ret['result'] = -3
+		ret['msg'] = 'need POST request.'
+	return JsonResponse(ret)
+@csrf_exempt
 def register(request):
+	ret = {
+		'result': 0,
+		'msg': ''
+	}
+	if request.method=='POST':
+		data = json.loads(request.body)
+		uname = data['username']
+		pwd = data['password']
+		group = data['group']
+		rname = data['realname']
+		if group=='Clerk':
+			jnum = data['jobnumber']
+			clerk = Clerk.objects.get_or_create(username=uname, password=pwd, group=group, realName=rname, jobnumber=jnum, lastLogin=datetime.datetime.fromtimestamp(0))
+			if clerk[1]:
+				ret['result'] = 1
+				ret['msg'] = 'clerk register successfully.'
+			else:
+				ret['result'] = -1
+				ret['msg'] = 'clerk already exist.'
+		elif group=='Customer':
+			phone = data['phone']
+			email = data['email']
+			customer = Customer.objects.get_or_create(username=uname, password=pwd, group=group, realName=rname, phone=phone, email=email, lastLogin=datetime.datetime.fromtimestamp(0))
+			if customer[1]:
+				ret['result'] = 1
+				ret['msg'] = 'customer register successfully.'
+			else:
+				ret['result'] = -1
+				ret['msg'] = 'customer already exist.'
+	else :
+		ret['result'] = -3
+		ret['msg'] = 'need POST request.'
+	return JsonResponse(ret)
+@csrf_exempt
+def changepwd(request):
 	pass
