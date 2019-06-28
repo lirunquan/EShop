5d287d52a9b1f31bf237cc3bc84fdd4bf5c7ff68
lirunquan <1617277400@qq.com>
2019-06-28 22:04:16 +0800

    use session

--- eshopproject/apps/user/views.py
+++ eshopproject/apps/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import Clerk, Customer
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.utils import timezone
 import datetime
 # Create your views here.
@@ -25,6 +25,8 @@ def login(request):
 				ret['msg'] = 'the customer does not exists.'
 			elif len(customer)==1 :
 				if customer[0].isOnline :
+					request.session['user_id'] = customer[0].username
+					request.session['user_group'] = customer[0].group
 					ret['result'] = 2
 					ret['msg'] = 'customer has logged in.'
 				else :	
@@ -33,6 +35,7 @@ def login(request):
 						customer[0].lastLogin = timezone.localtime(timezone.now())
 						customer[0].save()
 						request.session['user_id'] = customer[0].username
+						request.session['user_group'] = customer[0].group
 						ret['result'] = 1
 						ret['msg'] = 'customer login successfully.'
 					else:
@@ -53,6 +56,7 @@ def login(request):
 						clerk[0].lastLogin = timezone.localtime(timezone.now())
 						clerk[0].save()
 						request.session['user_id'] = clerk[0].username
+						request.session['user_group'] = clerk[0].group
 						ret['result'] = 1
 						ret['msg'] = 'clerk login successfully.'
 					else:
@@ -102,46 +106,50 @@ def changepwd(request):
 	ret = {'result': 0}
 	if request.method=='POST':
 		data = json.loads(request.body)
-		uname = data['username']
-		group = data['group']
-		oldpwd = data['old_password']
-		newpwd = data['new_password']
-		if group=='Customer' :
-			customer = Customer.objects.filter(username=uname)
-			if len(customer)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the customer does not exist.'
-			elif len(customer)==1 :
-				if customer[0].isOnline :
-					if customer[0]==oldpwd :
-						customer[0].password = newpwd
-						customer[0].save()
-						ret['result'] = 1
-						ret['msg'] = 'change password successfully.'
+		try:
+			uname = request.session['user_id']
+			group = request.session['user_group']
+		except Exception as e:
+			return JsonResponse({'request': -4, 'msg': 'needs login.'})
+		else:
+			oldpwd = data['old_password']
+			newpwd = data['new_password']
+			if group=='Customer' :
+				customer = Customer.objects.filter(username=uname)
+				if len(customer)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the customer does not exist.'
+				elif len(customer)==1 :
+					if customer[0].isOnline :
+						if customer[0]==oldpwd :
+							customer[0].password = newpwd
+							customer[0].save()
+							ret['result'] = 1
+							ret['msg'] = 'change password successfully.'
+						else :
+							ret['result'] = -1
+							ret['msg'] = 'incorrect password.'
 					else :
-						ret['result'] = -1
-						ret['msg'] = 'incorrect password.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the customer needs login.'
-		elif group=='Clerk' :
-			clerk = Clerk.objects.filter(username=uname)
-			if len(clerk)==0 :
-				ret['result'] = -1
-				ret['msg'] = 'the clerk does not exist.'
-			elif len(clerk)==1 :
-				if clerk[0].isOnline :
-					if clerk[0]==oldpwd :
-						clerk[0].password = newpwd
-						clerk[0].save()
-						ret['result'] = 1
-						ret['msg'] = 'change password successfully.'
+						ret['result'] = -4
+						ret['msg'] = 'the customer needs login.'
+			elif group=='Clerk' :
+				clerk = Clerk.objects.filter(username=uname)
+				if len(clerk)==0 :
+					ret['result'] = -1
+					ret['msg'] = 'the clerk does not exist.'
+				elif len(clerk)==1 :
+					if clerk[0].isOnline :
+						if clerk[0]==oldpwd :
+							clerk[0].password = newpwd
+							clerk[0].save()
+							ret['result'] = 1
+							ret['msg'] = 'change password successfully.'
+						else :
+							ret['result'] = -2
+							ret['msg'] = 'incorrect password.'
 					else :
-						ret['result'] = -2
-						ret['msg'] = 'incorrect password.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the clerk needs login.'
+						ret['result'] = -4
+						ret['msg'] = 'the clerk needs login.'
 	else:
 		ret['result'] = -5
 		ret['msg'] = 'need POST request.'
@@ -151,41 +159,45 @@ def checkinfo(request):
 	ret = {'result': 0}
 	if request.method=='POST' :
 		data = json.loads(request.body)
-		uname = data['username']
-		group = data['group']
-		if group=='Customer' :
-			customer = Customer.objects.filter(username=uname)
-			if len(customer)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the customer does not exist.'
-			elif len(customer)==1 :
-				if customer[0].isOnline :
-					ret['result'] = 1
-					ret['msg'] = 'show customer info.'
-					ret['info'] = {
-						'phone': customer[0].phone,
-						'email': customer[0].email,
-						'realname': customer[0].realName,
-						'account': customer[0].account
-					}
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the customer needs login.'
-		elif group=='Clerk' :
-			clerk = Clerk.objects.filter(username=uname)
-			if len(clerk)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the clerk does not exist.'
-			elif len(clerk)==1 :
-				if clerk[0].isOnline :
-					ret['result'] = 1
-					ret['msg'] = 'show clerk info.'
-					ret['info'] = {
-						'jobnumber': clerk[0].jobnumber
-					}
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the clerk needs login.'
+		try:
+			uname = request.session['user_id']
+			group = request.session['user_group']
+		except Exception as e:
+			return JsonResponse({'request': -4, 'msg': 'needs login.'})
+		else:
+			if group=='Customer' :
+				customer = Customer.objects.filter(username=uname)
+				if len(customer)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the customer does not exist.'
+				elif len(customer)==1 :
+					if customer[0].isOnline :
+						ret['result'] = 1
+						ret['msg'] = 'show customer info.'
+						ret['info'] = {
+							'phone': customer[0].phone,
+							'email': customer[0].email,
+							'realname': customer[0].realName,
+							'account': customer[0].account
+						}
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the customer needs login.'
+			elif group=='Clerk' :
+				clerk = Clerk.objects.filter(username=uname)
+				if len(clerk)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the clerk does not exist.'
+				elif len(clerk)==1 :
+					if clerk[0].isOnline :
+						ret['result'] = 1
+						ret['msg'] = 'show clerk info.'
+						ret['info'] = {
+							'jobnumber': clerk[0].jobnumber
+						}
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the clerk needs login.'
 	else:
 		ret['result'] = -5
 		ret['msg'] = 'need POST request.'
@@ -195,42 +207,46 @@ def changeinfo(request):
 	ret = {'result': 0}
 	if request.method=='POST' :
 		data = json.loads(request.body)
-		uname = data['username']
-		group = data['group']
-		if group=='Customer' :
-			customer = Customer.objects.filter(username=uname)
-			if len(customer)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the customer does not exist.'
-			elif len(customer)==1 :
-				if customer[0].isOnline :
-					if 'phone' in data :
-						customer[0].phone = data['phone']
-					if 'email' in data :
-						customer[0].email = data['email']
-					if 'realname' in data :
-						customer[0].realName = data['realname']
-					customer[0].save()
-					ret['result'] = 1
-					ret['msg'] = 'change customer info successfully.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the customer needs login.'
-		elif group=='Clerk' :
-			clerk = Clerk.objects.filter(username=uname)
-			if len(clerk)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the clerk does not exist.'
-			elif len(clerk)==1 :
-				if clerk[0].isOnline :
-					if 'jobnumber' in data :
-						clerk[0].realName = data['jobnumber']
-					clerk[0].save()
-					ret['result'] = 1
-					ret['msg'] = 'show clerk info.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the clerk needs login.'
+		try:
+			uname = request.session['user_id']
+			group = request.session['user_group']
+		except Exception as e:
+			return JsonResponse({'request': -4, 'msg': 'needs login.'})
+		else:
+			if group=='Customer' :
+				customer = Customer.objects.filter(username=uname)
+				if len(customer)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the customer does not exist.'
+				elif len(customer)==1 :
+					if customer[0].isOnline :
+						if 'phone' in data :
+							customer[0].phone = data['phone']
+						if 'email' in data :
+							customer[0].email = data['email']
+						if 'realname' in data :
+							customer[0].realName = data['realname']
+						customer[0].save()
+						ret['result'] = 1
+						ret['msg'] = 'change customer info successfully.'
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the customer needs login.'
+			elif group=='Clerk' :
+				clerk = Clerk.objects.filter(username=uname)
+				if len(clerk)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the clerk does not exist.'
+				elif len(clerk)==1 :
+					if clerk[0].isOnline :
+						if 'jobnumber' in data :
+							clerk[0].realName = data['jobnumber']
+						clerk[0].save()
+						ret['result'] = 1
+						ret['msg'] = 'show clerk info.'
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the clerk needs login.'
 	else:
 		ret['result'] = -5
 		ret['msg'] = 'need POST request.'
@@ -240,22 +256,26 @@ def charge(request):
 	ret = {'result': 0}
 	if request.method=='POST' :
 		data = json.loads(request.body)
-		uname = data['username']
-		group = data['group']
-		if group=='Customer' :
-			customer = Customer.objects.filter(username=uname)
-			if len(customer)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the customer does not exist.'
-			elif len(customer)==1 :
-				if customer[0].isOnline :
-					customer[0].account += data['amount']
-					customer[0].save()
-					ret['result'] = 1
-					ret['msg'] = 'charge successfully.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the customer needs login.'
+		try:
+			uname = request.session['user_id']
+			group = request.session['user_group']
+		except Exception as e:
+			return JsonResponse({'request': -4, 'msg': 'needs login.'})
+		else:
+			if group=='Customer' :
+				customer = Customer.objects.filter(username=uname)
+				if len(customer)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the customer does not exist.'
+				elif len(customer)==1 :
+					if customer[0].isOnline :
+						customer[0].account += data['amount']
+						customer[0].save()
+						ret['result'] = 1
+						ret['msg'] = 'charge successfully.'
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the customer needs login.'
 	else:
 		ret['result'] = -5
 		ret['msg'] = 'need POST request.'
@@ -265,38 +285,42 @@ def logout(request):
 	ret = {'result': 0}
 	if request.method=='POST' :
 		data = json.loads(request.body)
-		uname = data['username']
-		group = data['group']
-		if group=='Customer' :
-			customer = Customer.objects.filter(username=uname)
-			if len(customer)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the customer does not exist.'
-			if len(customer)==1 :
-				if customer[0].isOnline :
-					customer[0].isOnline = False
-					customer[0].save()
-					request.session.flush()
-					ret['result'] = 1
-					ret['msg'] = 'customer logout successfully.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the customer needs login.'
-		elif group=='Clerk' :
-			clerk = Clerk.objects.filter(username=uname)
-			if len(clerk)==0 :
-				ret['result'] = -2
-				ret['msg'] = 'the clerk does not exist.'
-			if len(clerk)==1 :
-				if clerk[0].isOnline :
-					clerk[0].isOnline = False
-					clerk[0].save()
-					request.session.flush()
-					ret['result'] = 1
-					ret['msg'] = 'clerk logout successfully.'
-				else :
-					ret['result'] = -4
-					ret['msg'] = 'the clerk needs login.'
+		try:
+			uname = request.session['user_id']
+			group = request.session['user_group']
+		except Exception as e:
+			return JsonResponse({'request': -4, 'msg': 'needs login.'})
+		else:
+			if group=='Customer' :
+				customer = Customer.objects.filter(username=uname)
+				if len(customer)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the customer does not exist.'
+				if len(customer)==1 :
+					if customer[0].isOnline :
+						customer[0].isOnline = False
+						customer[0].save()
+						request.session.flush()
+						ret['result'] = 1
+						ret['msg'] = 'customer logout successfully.'
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the customer needs login.'
+			elif group=='Clerk' :
+				clerk = Clerk.objects.filter(username=uname)
+				if len(clerk)==0 :
+					ret['result'] = -2
+					ret['msg'] = 'the clerk does not exist.'
+				if len(clerk)==1 :
+					if clerk[0].isOnline :
+						clerk[0].isOnline = False
+						clerk[0].save()
+						request.session.flush()
+						ret['result'] = 1
+						ret['msg'] = 'clerk logout successfully.'
+					else :
+						ret['result'] = -4
+						ret['msg'] = 'the clerk needs login.'
 	else :
 		ret['result'] = -3
 		ret['msg'] = 'need POST request.'
