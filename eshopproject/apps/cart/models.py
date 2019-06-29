9807d0402a1e388d427087c33ae901a17460e432
lirunquan <1617277400@qq.com>
2019-06-29 08:14:01 +0800

    change model

--- eshopproject/apps/cart/models.py
+++ eshopproject/apps/cart/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django_mysql.models import JSONField
 from apps.user.models import Customer
 # Create your models here.
+def df_json():
+	return {'list':[]}
 class Cart(models.Model):
 	customer = models.OneToOneField(Customer, on_delete=models.CASCADE, verbose_name='owner')
-	goodsList = JSONField()
+	goodsList = JSONField(default=df_json)
