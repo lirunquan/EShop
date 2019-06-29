205684f65240e6e8a523b05b23e8f2ef60197878
lirunquan <1617277400@qq.com>
2019-06-29 13:30:51 +0800

    fix goods view

--- eshopproject/apps/goods/views.py
+++ eshopproject/apps/goods/views.py
@@ -3,6 +3,9 @@ from .models import Goods
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from apps.cart.models import Cart
+from apps.user.models import Customer
+from django_mysql.models.functions import *
 # Create your views here.
 @csrf_exempt
 def goods_look(request):
@@ -26,6 +29,67 @@ def goods_look(request):
 		else :
 			ret['result'] = -2
 			ret['msg'] = 'no more goods.'
+	else:
+		ret['result'] = -5
+		ret['msg'] = 'needs POST request.'
+	return JsonResponse(ret)
+@csrf_exempt
+def addtocart(request):
+	ret = {'result':0}
+	if request.method=='POST':
+		try:
+			uname = request.session['user_id']
+			group = request.session['user_group']
+		except Exception as e:
+			return JsonResponse({'request': -4, 'msg': 'needs login.'})
+		else:
+			data = json.loads(request.body)
+			customer = Customer.objects.get(username=uname)
+			cart = Cart.objects.get(owner=customer)
+			i_code = data['isbncode']
+			name = data['name']
+			price = data['price']
+			number = data['number']
+			l = cart.goodsList['list']
+			if len(l)>0:
+				a = 0
+				for i in l:
+					if i['isbncode']==i_code:
+						i['number'] += 1
+						break
+					a++
+				if a==len(l):
+					cart.goodsList['list'].append(data)
+			else:
+				cart.goodsList['list'].append(data)
+
+	else:
+		ret['result'] = -5
+		ret['msg'] = 'needs POST request.'
+	return JsonResponse(ret)
+@csrf_exempt
+def goods_detail(request):
+	ret = {'result':0}
+	if request.method=='POST':
+		data = json.loads(request.body)
+		i_code = data['isbncode']
+		g = Goods.objects.filter(isbnCode=i_code)
+		if len(g)==0:
+			ret['result'] = -2
+			ret['msg'] = 'the goods does not exist.'
+		elif len(g)==1 :
+			ret['result'] = 1
+			ret['msg'] = 'show goods detail.'
+			ret['goods'] = {
+				'name': g[0].name,
+				'isbncode': g[0].isbnCode,
+				'price': g[0].price,
+				'params': g[0].params,
+				'picture': g[0].picture,
+				'detail': g[0].detail,
+				'isselling': g[0].isSelling,
+				'tags': g[0].tags
+			}
 	else:
 		ret['result'] = -5
 		ret['msg'] = 'needs POST request.'
