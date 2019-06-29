606476a5566ab0932f3d17aadce72cca06410c66
lirunquan <1617277400@qq.com>
2019-06-29 14:16:50 +0800

    add to cart

--- eshopproject/apps/goods/urls.py
+++ eshopproject/apps/goods/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
 	path('list', views.goods_look, name='list'),
-	path('detail', views.goods_detail, name='detail')
+	path('detail', views.goods_detail, name='detail'),
+	path('addtocart', views.addtocart, name='addtocart'),
 ]
