5bbfc0eade17623e5f7375fd2d51645b34fae646
lirunquan <1617277400@qq.com>
2019-07-04 01:04:41 +0800

    change goods models

--- eshopproject/apps/goods/models.py
+++ eshopproject/apps/goods/models.py
@@ -8,13 +8,14 @@ def pic_path_name(instance, filename):
 class Goods(models.Model):
 	isbnCode = models.CharField(max_length=13, unique=True, verbose_name='ISBN_code')
 	name = models.CharField(max_length=50, verbose_name='name')
-	price = models.FloatField(verbose_name='price')
+	cost = models.FloatField(verbose_name='进货价', default=0.0)
+	price = models.FloatField(verbose_name='price', default=0.0)
 	params = JSONField()
 	picture = models.ImageField(upload_to=pic_path_name, blank=True, default='goods/default.png')
 	detail = HTMLField(max_length=200, verbose_name='details(.html)')
 	repertory = models.PositiveIntegerField(verbose_name='repertory')
 	gType = models.CharField(max_length=20, verbose_name='goods_type')
-	isSelling = models.BooleanField(blank=False)
+	isSelling = models.BooleanField(default=False, blank=False)
 	tags = ListCharField(base_field=models.CharField(max_length=5), size=5, max_length=(5*6))
 	def __str__(self):
 		return '%s[%s]'%(self.name, self.isbnCode)
