4f661c73f19cd1d0e626d30c0e1ec72c70e906e5
lirunquan <1617277400@qq.com>
2019-07-05 00:02:04 +0800

    fix error

--- eshopproject/apps/user/views.py
+++ eshopproject/apps/user/views.py
@@ -474,8 +474,13 @@ def clerk_purchase(request):
 						cost = g['cost']
 						name = g['name']
 						count = g['number']
-						g_find = Goods.objects.get_or_create(isbnCode=i_code, name=name, cost=cost, repertory=count)
-						if g_find[1]==False :
+						g_find = Goods.objects.get_or_create(isbnCode=i_code)
+						if g_find[1] :
+							g_find[0].name = name
+							g_find[0].repertory = count
+							g_find[0].cost = cost
+							g_find[0].save()
+						else:
 							g_find[0].repertory += count
 							g_find[0].cost = cost
 							g_find[0].save()
