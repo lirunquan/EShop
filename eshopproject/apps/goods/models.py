72a6ccb2ec2dd155b0e6d93cfda3ba2ce7f88a65
lirunquan <1617277400@qq.com>
2019-07-06 22:30:11 +0800

    fix goods model

--- eshopproject/apps/goods/models.py
+++ eshopproject/apps/goods/models.py
@@ -10,14 +10,14 @@ class Goods(models.Model):
 	name = models.CharField(max_length=50, verbose_name='name')
 	cost = models.FloatField(verbose_name='进货价', default=0.0)
 	price = models.FloatField(verbose_name='price', default=0.0)
-	producer = models.TextField(default='', null=True)
+	producer = models.TextField(default='', blank=True, null=True)
 	params = JSONField(blank=True, null=True)
 	picture = models.ImageField(upload_to=pic_path_name, blank=True, default='goods/default.png')
 	detail = HTMLField(max_length=200, verbose_name='details(.html)', blank=True, null=True)
 	repertory = models.PositiveIntegerField(verbose_name='repertory', default=0)
 	gType = models.CharField(max_length=20, verbose_name='goods_type', blank=True, null=True)
 	isSelling = models.BooleanField(default=False, blank=False)
-	tags = ListCharField(base_field=models.CharField(max_length=5), size=5, max_length=(5*6), null=True)
+	#tags = ListCharField(base_field=models.CharField(max_length=5, null=True), size=5, max_length=(5*6), null=True)
 	def __str__(self):
 		return '%s[%s]'%(self.name, self.isbnCode)
 	
